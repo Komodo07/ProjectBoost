45e4da29f33a9b3fc96433027bc62b987a2d85f6
Jason Tucker <komodo.07@outlook.com>
2024-04-24 23:17:49 -0500

    Added Camera and Environment
    Added Camera and Environment to scene and modified code in Player script.

--- player.cs
+++ player.cs
@@ -3,20 +3,12 @@ using System;
 
 public partial class player : Node3D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		GD.Print("Hello World!");
-		GD.Print("Don't panic");
-		GD.Print("42");
-	}
-
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (Input.IsActionPressed("ui_accept"))
+		if (Input.IsActionJustPressed("ui_accept"))
 		{
-
-		}
+			
+		}		
 	}
 }
